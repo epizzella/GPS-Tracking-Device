443dc591102815157629e94dd601f06ec1ffdb69
Ed Pizzella <ed.pizzella@gmail.com>
2024-10-23 17:36:46 -0400

    Update for software timer testing

--- source/main.zig
+++ source/main.zig
@@ -8,6 +8,7 @@ const EventGroup = Os.EventGroup;
 const OsError = Os.OsError;
 const Sem = Os.Semaphore;
 const MsgQueue = Os.createMsgQueueType(.{ .MsgType = LedMsg, .buffer_size = 3 });
+const Timer = Os.Timer;
 
 const blink_time = 500;
 
@@ -16,16 +17,34 @@ const TestType = enum {
     semaphore,
     event_group,
     msg_q,
+    timer,
 };
 
-const test_type = TestType.event_group;
+const test_type = TestType.timer;
 
 var led_mutex = Mutex.create_mutex("led_mutex");
 var sem = Sem.create_semaphore(.{ .name = "led_semaphore", .inital_value = 2 });
 var event_group = EventGroup.createEventGroup(.{ .name = "led_event_group" });
 var msg_queue = MsgQueue.createQueue(.{ .name = "led_msg", .inital_val = LedMsg.off });
+var timer = Timer.create(.{ .name = "led_timer", .callback = &timerCallback });
+var timer2 = Timer.create(.{ .name = "led_timer2", .callback = &timerCallback2 });
 
 const LedMsg = enum { toggle, on, off };
+
+var timerLed: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_10 };
+
+var led1: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_6 };
+var led2: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_8 };
+var led3: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_9 };
+var led4: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_10 };
+
+fn timerCallback() void {
+    led1.TogglePin();
+}
+
+fn timerCallback2() void {
+    led2.TogglePin();
+}
 
 fn idleTask() !void {
     var led = zgpio{ .m_port = hal.GPIOC, .m_pin = hal.GPIO_PIN_13 };
@@ -36,40 +55,45 @@ fn idleTask() !void {
 }
 
 fn task1() !void {
-    var myLed: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_6 };
     while (true) {
         switch (test_type) {
             .event_group => {
                 _ = try event_group.awaitEvent(.{ .event_mask = 0b01, .PendOn = .any_set });
                 try event_group.writeEvent(.{ .event = 0b00 });
-                myLed.TogglePin();
+                led1.TogglePin();
             },
             .msg_q => {
                 const msg = try msg_queue.awaitMsg(.{});
                 switch (msg) {
-                    .on => myLed.WritePin(.Set),
-                    .off => myLed.WritePin(.Reset),
-                    .toggle => myLed.TogglePin(),
+                    .on => led1.WritePin(.Set),
+                    .off => led1.WritePin(.Reset),
+                    .toggle => led1.TogglePin(),
                 }
             },
             .mutex => {
-                try mutex_blink(&myLed);
+                try mutex_blink(&led1);
             },
             .semaphore => {
-                try semaphore_blink(&myLed);
+                try semaphore_blink(&led1);
+            },
+            .timer => {
+                try timer.set(.{ .autoreload = true, .timeout_ms = 250 });
+                try timer2.set(.{ .autoreload = true, .timeout_ms = 500 });
+                try timer.start();
+                try timer2.start();
+                try tcb1.suspendMe();
             },
         }
     }
 }
 
 fn task2() !void {
-    var myLed: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_8 };
     while (true) {
         switch (test_type) {
             .event_group => {
                 _ = try event_group.awaitEvent(.{ .event_mask = 0b10, .PendOn = .any_set });
                 try event_group.writeEvent(.{ .event = 0b00 });
-                myLed.TogglePin();
+                led2.TogglePin();
             },
             .msg_q => {
                 try msg_queue.pushMsg(LedMsg.off);
@@ -78,17 +102,19 @@ fn task2() !void {
                 try Os.Time.delay(blink_time);
             },
             .mutex => {
-                try mutex_blink(&myLed);
+                try mutex_blink(&led2);
             },
             .semaphore => {
-                try semaphore_blink(&myLed);
+                try semaphore_blink(&led2);
+            },
+            .timer => {
+                try tcb2.suspendMe();
             },
         }
     }
 }
 
 fn task3() !void {
-    var myLed: zgpio = .{ .m_port = hal.GPIOA, .m_pin = hal.GPIO_PIN_9 };
     while (true) {
         switch (test_type) {
             .event_group => {
@@ -101,10 +127,13 @@ fn task3() !void {
                 try tcb3.suspendMe();
             },
             .mutex => {
-                try mutex_blink(&myLed);
+                try mutex_blink(&led3);
             },
             .semaphore => {
-                try semaphore_blink(&myLed);
+                try semaphore_blink(&led3);
+            },
+            .timer => {
+                try tcb3.suspendMe();
             },
         }
     }
@@ -120,10 +149,10 @@ fn mutex_blink(gpio: *zgpio) !void {
 }
 
 fn semaphore_blink(gpio: *zgpio) !void {
-    try sem.acquire(.{});
+    try sem.wait(.{});
     gpio.TogglePin();
     try Os.Time.delay(blink_time);
-    try sem.release();
+    try sem.post(.{});
     try Os.Time.delay(1);
 }
 
@@ -171,16 +200,21 @@ export fn main() void {
     tcb2.init();
     tcb3.init();
 
-    sem.init();
-    event_group.init();
-    msg_queue.init();
-    led_mutex.init();
+    sem.init() catch {};
+    event_group.init() catch {};
+    msg_queue.init() catch {};
+    led_mutex.init() catch {};
 
     Os.init();
     Os.startOS(.{
         .idle_task_subroutine = &idleTask,
         .idle_stack_size = 25,
-        .sysTick_callback = &incTick,
+        .os_tick_callback = &incTick,
+        .timer_config = .{
+            .timer_enable = true,
+            .timer_stack_size = 100,
+            .timer_task_priority = 0,
+        },
     });
 
     unreachable;
